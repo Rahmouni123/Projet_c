6ad6096e0f9fa619aed082e32ffedce300457518
FranklinB13 <153076947+FranklinB13@users.noreply.github.com>
2024-01-04 18:58:12 +0100

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Structure pour les transitions
+typedef struct {
+    int initial_state;
+    int final_state;
+} Transition;
+
+// Structure pour les listes de symboles
+struct Noeud {
+    int valeur;
+    struct Noeud *suivant;
+};
+typedef struct Noeud *Noeud;
+
+typedef struct {
+    int nbE;
+    int nSy;
+    int transition;
+    int ***mat;
+} Automate;
+
+
 int is_complete(Automaton *automaton) {
     for (int i = 0; i < ALPHABET_SIZE; i++) {
         char letter = automaton->alphabet[i];
